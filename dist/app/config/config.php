48fbff8d0827470092ab82bb213829ad924e51a9
academypro01 <80899303+academypro01@users.noreply.github.com>
2021-10-05 23:14:52 +0330

    fix bugs

--- dist/app/config/config.php
+++ dist/app/config/config.php
@@ -5,7 +5,7 @@ if(session_id() == ''){
 }
 date_default_timezone_set("Asia/Tehran");
 // database information
-define('DB_HOST', '');
+define('DB_HOST', 'localhost');
 define('DB_USER', '');
 define('DB_PASS', '');
 define('DB_NAME', '');
@@ -17,6 +17,11 @@ define('SMTP_PORT','');
 define('SMTP_PASS','');
 define('SMTP_NAME','LinkShortner System');
 
+// set local root path
 define('APPROOT', realpath(dirname(dirname(__FILE__))));
+
+// set url root path link
 define('URLROOT', '');
+
+// set site name
 define('SITENAME', 'Link Shortner System');
